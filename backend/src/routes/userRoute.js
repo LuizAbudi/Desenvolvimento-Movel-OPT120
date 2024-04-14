e491cab78a4d3149e33f8e2dd7c0a6b107d1b20a
LuizAbudi <luizabudi@hotmail.comgit config --global user.email luizabudi@hotmail.com>
2024-04-14 20:17:05 -0300

    fix: change route users

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -3,23 +3,23 @@ const { Router } = require('express');
 const router = Router();
 const userController = require('../../src/controllers/userController');
 
-router.get('/user', (req, res) => {
+router.get('/users', (req, res) => {
   userController.getAllUsers(req, res);
 });
 
-router.get('/user/:id', (req, res) => {
+router.get('/users/:id', (req, res) => {
   userController.getUsersById(req, res);
 });
 
-router.post('/user', (req, res) => {
+router.post('/users', (req, res) => {
   userController.create(req, res);
 });
 
-router.put('/user/:id', (req, res) => {
+router.put('/users:id', (req, res) => {
   userController.update(req, res);
 });
 
-router.delete('/user/:id', (req, res) => {
+router.delete('/users/:id', (req, res) => {
   userController.delete(req, res);
 });
 
